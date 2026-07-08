ccd17fffaa6c632fb8cb0ce19bc8e521914e0bb5
chriselement <chris@everybitmatters.com>
2026-07-08 16:02:49 -0400

    Update Lighthouse hydration feed

--- lighthouse-hydration.js
+++ lighthouse-hydration.js
@@ -65,13 +65,13 @@ receiveHydrationTracker({
   "images": [
     {
       "text": "Demo Image",
-      "url": "https://v5.airtableusercontent.com/v3/u/55/55/1783548000000/rV8SU9k2_7InlucDlSqOQQ/-3gtk8OxdCU1i45yqkUurYtx2kxS96lw7kvxDaBbDgH13L_xVT_vvNbmASwBmIBYhNeWVaGc2fHbfAELvwIutcvN2OS3KxGeP0F7P7he_KxHTf-S2czdbjLhuXP-TCDeJ14LSWebgmXvQISmgvgNvw6lKAVViN2wAneSpqwsJzs/NJ5QgCtH-GRuJ6rH1MFurOztO0qCIMyeRapNOOWgC98",
+      "url": "https://v5.airtableusercontent.com/v3/u/55/55/1783555200000/iDtUeCg9VJQYnLcuNXk21g/1YiviKiFGyXhFZdAHco5iQIgnQqsSGs1pcyf4BACVGg6Xn2Ykui7rVDyAvUoDlR2mk3V5gb7BXwky-YaQ5-GfcpZGwPa77MILW_osJEDmT2buztvbNVvRKo6WHsEtCnygo6rL0TFzbCuYBDQ6WZlE3bkoGVmkz3LoZPA1-Vursc/VJprZYgNp0R5ZSpqcF6cPbNJmU7K1aBpfvMImBNxv04",
       "filename": "house_example.png",
       "recordId": "rec3lpLZJPPLr3ki0"
     },
     {
       "text": "Photo of New Hampshire",
-      "url": "https://v5.airtableusercontent.com/v3/u/55/55/1783548000000/MumhgHM8w55ruuxefaMfOg/DcANha-DnbJzKGC7EF455hr8wzy4cNrEYXo6XA2-N3iH4DUzXJYm2BQgKpUytcJhFYdvo3iUwbZdjhEctyxcf_6V6f4HBQcNCRAOWJdQBVL9G7gyUUeyZATksrYOtG02h7FAmwTdGKjmfvOtKuK24Q/VezrhZhWKYZdPHmEdhZzBHzVyKq3kmJ9JP0FvBUAODE",
+      "url": "https://v5.airtableusercontent.com/v3/u/55/55/1783555200000/zz0TkTsya2XKpmU82JnyjQ/RLr1fBCJqQUBxHvXWL8Z7wZ-EWTfW5ZIZGzgIaMY4xtPiMOX1-GqyXAld58Mf-rCH5r7MOKxT-yq_POuAYBf8nDYBvWf1ndLs7JUseTQBQd0L7SzCym29GU2HrE_aAgcV__vHMYLOXBq2wijOT7gqg/jvCgWw3ZS1dkz7VjlWVdxfmz-OJD0YoI_9ofhMdtXuc",
       "filename": "NH_2.jpg",
       "recordId": "recNohKlU2aXeFCwg"
     }
@@ -81,5 +81,5 @@ receiveHydrationTracker({
     "ounces": 20,
     "submittedAt": "2026-07-07T15:06:44.000Z"
   },
-  "updatedAt": "2026-07-08T19:57:49.642Z"
+  "updatedAt": "2026-07-08T20:02:48.470Z"
 });
