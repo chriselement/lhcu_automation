8865dbb6e8724d0476c3e9d074b30c24943a1238
chriselement <chris@everybitmatters.com>
2026-07-20 09:57:48 -0400

    Update Lighthouse hydration feed

--- lighthouse-hydration.js
+++ lighthouse-hydration.js
@@ -2,14 +2,14 @@ receiveHydrationTracker({
   "success": true,
   "campaignTitle": "Help us fill the bottle!",
   "campaignSubtitle": "Every ounce logged adds to the splash.",
-  "totalOunces": 116,
+  "totalOunces": 0,
   "weekOunces": 0,
-  "participants": 1,
+  "participants": 0,
   "display": {
     "total": {
-      "value": "116",
+      "value": "0",
       "unit": "oz",
-      "subtext": "116 oz logged to date"
+      "subtext": "0 oz logged to date"
     },
     "week": {
       "value": "0",
@@ -18,11 +18,11 @@ receiveHydrationTracker({
     }
   },
   "bottle": {
-    "fillPercent": 23.200000000000003,
+    "fillPercent": 0,
     "nextMilestone": "500 oz"
   },
   "content": {
-    "showImages": true,
+    "showImages": false,
     "campaignTitle": "Help us fill the bottle!",
     "campaignSubtitle": "Every ounce logged adds to the splash.",
     "topCardHeader": "LET'S GET HYDRATED!",
@@ -62,24 +62,7 @@ receiveHydrationTracker({
       }
     ]
   },
-  "images": [
-    {
-      "text": "Demo Image",
-      "url": "https://v5.airtableusercontent.com/v3/u/55/55/1784563200000/56OVgVEukqlueq4ax6jeGA/wwe_sTNx_CvTuMgMFA33llZgRCDW7TgxJ1qy-E0ZRzFZlBJZ0ujS2tzOT0-4BUU8Zppx2L-MGDUTfvQJIYXRn2MGt32H3UrQeXMdy9Bh-rEbq5z62JLb6CDwyRgkxgcYS0pmJqNO7gVL71c0sluXSZtIr9u8h_qfQhq3B9jTu2E/QFF811rVrrlenpmEBvqzUI99xNcJ6c_xZ6SdeJzqh1I",
-      "filename": "house_example.png",
-      "recordId": "rec3lpLZJPPLr3ki0"
-    },
-    {
-      "text": "Photo of New Hampshire",
-      "url": "https://v5.airtableusercontent.com/v3/u/55/55/1784563200000/-r1BseOdevBke7EotNMcGQ/99HnX58-J0hI1O_dQioF1e3AIkD6BjAVGtOHNjKpnD2MiBkiPA8kU9DxLqlrPd5vXaVOj4F2L08243J8hBFYI06SZ-OizUbWE4X14ct6rJx6WknUYiI6iD1IsZTRYT77X_iD1OYQSYMGff4QSh9Vmw/k2KLgIPKzduN99as4o7DGLX3erZSPMGCycOKD3k562E",
-      "filename": "NH_2.jpg",
-      "recordId": "recNohKlU2aXeFCwg"
-    }
-  ],
-  "latestEntry": {
-    "name": "Lighthouse HQ",
-    "ounces": 20,
-    "submittedAt": "2026-07-07T15:06:44.000Z"
-  },
-  "updatedAt": "2026-07-20T13:52:48.928Z"
+  "images": [],
+  "latestEntry": null,
+  "updatedAt": "2026-07-20T13:57:47.973Z"
 });
