dc3da3f976d0913ee83a991789425da70069bc04
chriselement <chris@everybitmatters.com>
2026-08-21 17:22:49 -0400

    Update Lighthouse hydration feed

--- lighthouse-hydration.js
+++ lighthouse-hydration.js
@@ -68,5 +68,5 @@ receiveHydrationTracker({
     "ounces": 165982,
     "submittedAt": "2026-08-20T20:38:47.000Z"
   },
-  "updatedAt": "2026-08-21T21:17:48.732Z"
+  "updatedAt": "2026-08-21T21:22:49.101Z"
 });
